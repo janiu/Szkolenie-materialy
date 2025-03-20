bbbdc4134f76acd3e8a8ad19c19f3346ddc71cc8
janiu <janiu25@gmail.com>
2025-03-20 12:33:30 +0100

    Add files via upload

---
+++ Rozwiazania/04-fraud-detector/credit-card-monitor/src/main/java/com/org/rjankowski/kafka/creditcardmonitor/CreditCardMonitorApplication.java
@@ -0,0 +1,13 @@
+package com.org.rjankowski.kafka.creditcardmonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditCardMonitorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CreditCardMonitorApplication.class, args);
+    }
+
+}
